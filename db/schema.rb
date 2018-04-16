2b8e6c221329f11dc71999c66a52a2927e4b0cb0
Yoshi <crispy245@gmail.com>
2018-04-16 22:31:30 +0900

    Create users table and model
    Section 6, Lecture 92

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_10_131915) do
+ActiveRecord::Schema.define(version: 2018_04_16_132634) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
